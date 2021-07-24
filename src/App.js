3bd50431348e74e07b2e469bf8a63456789e3b77
Zedfeld Businos <nxt.fe.dev.two@gmail.com>
2021-07-24 16:58:24 +0800

    Clean app.js

--- src/App.js
+++ src/App.js
@@ -4,78 +4,12 @@ import {
     Route,
     Link
 } from "react-router-dom";
-import ThriftBrandIcon from './components/Icons/ThriftBrandIcon'
-import HomeIcon from './components/Icons/HomeIcon'
-import UserIcon from './components/Icons/UserIcon'
-import Home from "./views/Home";
-import SetupBudgetForm from "./views/SetupBudgetForm";
+
 
 function App() {
     return (
-        <div className="App">
-            <div className="">
-                <div className="flex overflow-hidden"> 
-                    <Router>
-                        {/* this is the navbar duuhhhhh?? */}
-                        <Navbar />
-
-                        {/* this is where everything get's rendered, the big white space  */}
-                        <div className="w-full bg-thrift-lightest bg-opacity-10 pt-8 px-10 h-screen overflow-auto">
-                            <Switch>
-                                <Route exact path="/">
-                                    <Home />
-                                </Route>
-                                <Route path="/account">
-                                    <h1>Account page</h1>
-                                </Route>
-                                <Route path="/budget/set-up">
-                                   <SetupBudgetForm />
-                                </Route>
-                            </Switch>
-                        </div>
-                    </Router>
-                </div>
-            </div>
-        </div>
-    );
+        <div>Hello World</div>
+    ); 
 }
-
-function Navbar() {
-    const brandIcon = <ThriftBrandIcon />
-    const userIcon = <UserIcon />
-    const homeIcon = <HomeIcon />
-    return (
-        <div className="h-screen w-20 bg-thrift text-thrift-highlight px-1 py-8">
-            <nav className="flex flex-col items-center justify-between h-full">
-                {/* brand */}
-                <div>
-                    <Link to="/">
-                        <div className="p-2 hover:bg-thrift-highlight hover:bg-opacity-30 rounded-full">
-                            {brandIcon}
-                        </div>
-                    </Link>
-                </div>
-                {/* nav items  */}
-                <div className="flex flex-col items-center space-y-5">
-                    <Link to="/">
-                        <NavbarLink linkIcon={homeIcon} />
-                    </Link>
-                    <Link to="/account">
-                        <NavbarLink linkIcon={userIcon} />
-                    </Link>
-                </div>
-            </nav>
-        </div>
-    )
-}
-
-function NavbarLink(props) {
-    return (
-        <div className="w-10 h-10 p-2 hover:bg-thrift-highlight hover:bg-opacity-30 rounded-full">
-            {props.linkIcon}
-        </div>
-    )
-}
-
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
     mode: 'jit',
-    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+    purge: [
+        './src/**/*.{js,jsx,ts,tsx}', 
+        './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
